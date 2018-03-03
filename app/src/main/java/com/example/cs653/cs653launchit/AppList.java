521907652775d5d66ab2c24fa0eef47956acc687
Jeyasoorya <jeys041297sts@gmail.com>
2018-03-03 16:15:58 +0530

    Started Project

---
+++ app/src/main/java/com/example/cs653/cs653launchit/AppList.java
@@ -0,0 +1,13 @@
+package com.example.cs653.cs653launchit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_list);
+    }
+}
